66214e3c9b6ef700debd897a549c077e0ed4b713
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-07 16:36:14 +0300

    Add Elevted Button

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,13 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random idea: by steve'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(onPressed: (){
+            print("button  pressed");
+             },
+              child: Text("Next")
+             ),
         ],
       ),
     );
