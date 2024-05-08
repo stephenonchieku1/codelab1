4518fdc1a8b1616b2637a2b15099971e8cd75cee
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-08 14:56:30 +0300

    apply styling to text

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class MyWidget extends StatelessWidget {
@@ -6,12 +5,48 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
-      padding: EdgeInsets.fromLTRB(10, 40,0,0),
-      //width: 200,
-      //height: 100,
-      child:  Text("hello",style: TextStyle(fontSize: 50),),
+    return Column(
+      children: [
+        Container(
+          color: Colors.brown[200],
+          padding: EdgeInsets.all(20),
+          child: Text('how old are you'),
+        ),
+        Container(
+          color: Colors.brown[200],
+          padding: EdgeInsets.all(20),
+          child: Text('whats your  name'),
+        ),
+        Container(
+          color: Colors.brown[200],
+          padding: EdgeInsets.all(20),
+          child: Text(''),
+        )
+      ],
+    );
+  }
+}
+
+class Rows extends StatelessWidget {
+  const Rows({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: Colors.white,
+          child: Text('hello world',style: TextStyle(color: Colors.black ),),
+        ),
+        Container(
+          color: Colors.blue[200],
+          padding: EdgeInsets.all(20),
+          child: Text('John Doe' ,style: TextStyle(color: Colors.blue[800]),
+        )
+        )
+      ],
 
     );
   }
